363cfa9d60dc9ffc65ecd9c78452f3132b744975
D-xuanmo <me@xuanmo.xin>
2023-07-08 17:54:51 +0800

    fix: 修复 URL 规则无法校验完整 URL 问题

--- src/rules/url.ts
+++ src/rules/url.ts
@@ -1,7 +1,7 @@
 import { SingleRuleType } from '../types'
 
 const rule: SingleRuleType = {
-  validator: (value: unknown) => /^https?:\/\/(\w+\.)+\w+(:\d+)?(\/|\?.*)?$/i.test(value as string)
+  validator: (value: unknown) => /^https?:\/\/(\w+\.)+\w+(:\d+)?([?\/]\S*)?$/i.test(value as string)
 }
 
 export default rule
